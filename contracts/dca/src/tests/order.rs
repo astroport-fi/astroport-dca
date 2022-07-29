00981d41b16044f3979ee07b6ba96b782103d34d
Pakorn Nathong <goddesign11294_2@hotmail.com>
2022-07-30 01:03:39 +0700

    feat: add create multiple orders test

--- contracts/dca/src/tests/order.rs
+++ contracts/dca/src/tests/order.rs
@@ -138,3 +138,115 @@ fn create_order() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+#[test]
+fn create_multiple_orders_same_asset() -> Result<(), Box<dyn Error>> {
+    let (mut app, dca) = instantiate();
+
+    app.execute_contract(
+        Addr::unchecked(USER_ONE),
+        dca.clone(),
+        &ExecuteMsg::CreateDcaOrder {
+            initial_asset: native_asset(USDC, 50_000_000),
+            target_asset: native_info(LUNA),
+            interval: 600,
+            dca_amount: Uint128::new(10_000_000),
+            start_at: None,
+        },
+        &[Coin::new(50_000_000, USDC)],
+    )?;
+
+    app.execute_contract(
+        Addr::unchecked(USER_ONE),
+        dca.clone(),
+        &ExecuteMsg::CreateDcaOrder {
+            initial_asset: native_asset(USDC, 50_000_000),
+            target_asset: native_info(USDT),
+            interval: 600,
+            dca_amount: Uint128::new(10_000_000),
+            start_at: None,
+        },
+        &[Coin::new(50_000_000, USDC)],
+    )?;
+
+    let infos: Vec<UserDcaInfo> = app.wrap().query_wasm_smart(
+        &dca,
+        &QueryMsg::UserDcaOrders {
+            user: USER_ONE.to_string(),
+        },
+    )?;
+
+    assert_eq!(infos.len(), 2);
+
+    let mut infos_iter = infos.into_iter();
+    let second_order = infos_iter.next().unwrap();
+    assert_eq!(second_order.info.id, 1);
+    assert_eq!(
+        second_order.info.initial_asset,
+        native_asset(USDC, 50_000_000)
+    );
+    let first_order = infos_iter.next().unwrap();
+    assert_eq!(first_order.info.id, 0);
+    assert_eq!(
+        first_order.info.initial_asset,
+        native_asset(USDC, 50_000_000)
+    );
+
+    Ok(())
+}
+
+#[test]
+fn create_multiple_orders_diff_asset() -> Result<(), Box<dyn Error>> {
+    let (mut app, dca) = instantiate();
+
+    app.execute_contract(
+        Addr::unchecked(USER_ONE),
+        dca.clone(),
+        &ExecuteMsg::CreateDcaOrder {
+            initial_asset: native_asset(USDC, 50_000_000),
+            target_asset: native_info(LUNA),
+            interval: 600,
+            dca_amount: Uint128::new(10_000_000),
+            start_at: None,
+        },
+        &[Coin::new(50_000_000, USDC)],
+    )?;
+
+    app.execute_contract(
+        Addr::unchecked(USER_ONE),
+        dca.clone(),
+        &ExecuteMsg::CreateDcaOrder {
+            initial_asset: native_asset(USDT, 50_000_000),
+            target_asset: native_info(LUNA),
+            interval: 600,
+            dca_amount: Uint128::new(10_000_000),
+            start_at: None,
+        },
+        &[Coin::new(50_000_000, USDT)],
+    )?;
+
+    let infos: Vec<UserDcaInfo> = app.wrap().query_wasm_smart(
+        &dca,
+        &QueryMsg::UserDcaOrders {
+            user: USER_ONE.to_string(),
+        },
+    )?;
+
+    assert_eq!(infos.len(), 2);
+
+    let mut infos_iter = infos.into_iter();
+    let second_order = infos_iter.next().unwrap();
+    assert_eq!(second_order.info.id, 1);
+    assert_eq!(
+        second_order.info.initial_asset,
+        native_asset(USDT, 50_000_000)
+    );
+    let first_order = infos_iter.next().unwrap();
+    assert_eq!(first_order.info.id, 0);
+    assert_eq!(
+        first_order.info.initial_asset,
+        native_asset(USDC, 50_000_000)
+    );
+
+    Ok(())
+}
